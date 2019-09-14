439546500a0368d2f437484abebac3797dccb62a
Patryk Wielopolski <patryk.wielopolski@datawalk.com>
2019-09-14 18:48:36 +0200

    Automatized hyper param tuning using SA for different learners

--- xgb_param_tuning.R
+++ xgb_param_tuning.R
@@ -5,29 +5,60 @@ library(mlr)
 library(parallelMap)
 
 source("aucpr_measure.R")
-parallelStartMulticore(3)
+parallelStartMulticore(8)
 
-task <- makeClassifTask(id="CE-xgboost", data = df, target = "TARGET", positive = 1)
-learner <- makeLearner("classif.xgboost", predict.type = "prob")
+df <- read_csv("data/PCA_Insurance_Frauds.csv")
+df$TARGET <- factor(df$TARGET)
 
-xgb_params <- makeParamSet(
-  # The number of trees in the model (each one built sequentially)
-  makeIntegerParam("nrounds", lower = 100, upper = 500),
-  # number of splits in each tree
-  makeIntegerParam("max_depth", lower = 1, upper = 10),
-  # "shrinkage" - prevents overfitting
-  makeNumericParam("eta", lower = .1, upper = .5),
-  # L2 regularization - prevents overfitting
-  makeNumericParam("lambda", lower = -1, upper = 0, trafo = function(x) 10^x)
-)
-control <- makeTuneControlRandom(maxit = 1)
-resample_desc <- makeResampleDesc("CV", iters = 2)
+task <-
+  makeClassifTask(
+    id = "CE-xgboost",
+    data = df,
+    target = "TARGET",
+    positive = 1
+  )
+standard_xgb_learner <-
+  makeLearner("classif.xgboost", predict.type = "prob")
 
-tunedParams <- tuneParams(learner = learner,
-                          task = task,
-                          resampling = resample_desc,
-                          measures = aucpr,
-                          par.set = xgb_params,
-                          control=control)
+get.tuned.params <- function(df, task, learner) {
+  xgb_params <- makeParamSet(
+    # The number of trees in the model (each one built sequentially)
+    makeIntegerParam("nrounds", lower = 100, upper = 500),
+    # number of splits in each tree
+    makeIntegerParam("max_depth", lower = 1, upper = 10),
+    # "shrinkage" - prevents overfitting
+    makeNumericParam("eta", lower = .1, upper = .5),
+    # L2 regularization - prevents overfitting
+    makeNumericParam(
+      "lambda",
+      lower = -1,
+      upper = 0,
+      trafo = function(x)
+        10 ^ x
+    )
+  )
+  control <- makeTuneControlGenSA(budget = 30)
+  resample_desc <- makeResampleDesc("CV", iters = 2)
+  
+  tunedParams <- tuneParams(
+    learner = learner,
+    task = task,
+    resampling = resample_desc,
+    measures = aucpr,
+    par.set = xgb_params,
+    control = control
+  )
+  return(tunedParams)
+}
 
+learners <- c(standard_xgb_learner)
+
+tune_results <-
+  lapply(learners, function(learner) {
+    get.tuned.params(df = df,
+                     task = task,
+                     learner = learner)
+  })
+
+save(tune_results, file="tune_results.Rdata")
 parallelStop()
