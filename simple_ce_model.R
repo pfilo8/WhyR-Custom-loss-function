7a99f3fe53a325a2ef09d2f9dd2db96f7135ab0d
Bartosz Kolasa <bartosz.michal.kolasa@gmail.com>
2019-09-14 16:14:13 +0200

    Simple MLR learning tasks

---
+++ simple_ce_model.R
@@ -0,0 +1,29 @@
+library(xgboost)
+library(readr)
+library(dplyr)
+library(mlr)
+library(parallelMap)
+parallelStartMulticore(3)
+
+df <- read_csv("data/PCA_Insurance_Frauds.csv")
+df$TARGET <- factor(df$TARGET)
+
+task <- makeClassifTask(id="CE-xgboost", data = df, target = "TARGET", positive = 1)
+learner <- makeLearner("classif.xgboost", predict.type = "prob",
+            par.vals = list(
+              max_depth = 4,
+              eta = 0.01,
+              nthread = 4,
+              nrounds = 500,
+              objective = "binary:logistic"))
+
+measures <- list(mlr::auc, mmce)
+sampling_startegy <- makeResampleDesc("CV", iters=2, stratify = TRUE)
+
+bmr <- benchmark(list(learner), tasks = task, resamplings = sampling_startegy, measures = measures)
+bmr$results
+perf_data <- generateThreshVsPerfData(bmr, measures = list(tpr, ppv), gridsize = 1000)
+perf_data_auc <- generateThreshVsPerfData(bmr, measures = list(fpr, tpr), gridsize = 1000)
+plotROCCurves(perf_data_auc)
+
+parallelStop()
---
+++ xgb_param_tuning.R
@@ -0,0 +1,22 @@
+library(xgboost)
+library(readr)
+library(dplyr)
+library(mlr)
+library(parallelMap)
+parallelStartMulticore(3)
+
+task <- makeClassifTask(id="CE-xgboost", data = df, target = "TARGET", positive = 1)
+learner <- makeLearner("classif.xgboost", predict.type = "prob")
+
+xgb_params <- makeParamSet(
+  # The number of trees in the model (each one built sequentially)
+  makeIntegerParam("nrounds", lower = 100, upper = 500),
+  # number of splits in each tree
+  makeIntegerParam("max_depth", lower = 1, upper = 10),
+  # "shrinkage" - prevents overfitting
+  makeNumericParam("eta", lower = .1, upper = .5),
+  # L2 regularization - prevents overfitting
+  makeNumericParam("lambda", lower = -1, upper = 0, trafo = function(x) 10^x)
+)
+control <- makeTuneControlRandom(maxit = 1)
+resample_desc <- makeResampleDesc("CV", iters = 2)
